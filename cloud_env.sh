d6e4a29808a21dba65e1f302720b2b6b832aae9d
Iryna <37odri@gmail.com>
2022-12-08 01:50:32 +0200

    .

--- cloud_env.sh
+++ cloud_env.sh
@@ -1,7 +1,7 @@
 printenv > .env
 
+sed -i "s/%DATABASE_URL%/"$DATABASE_URL"/g" app.yaml
 sed -i "s/%JWT_SECRET%/"$JWT_SECRET"/g" app.yaml
 sed -i "s/%STAGE%/"$STAGE"/g" app.yaml
-sed -i "s/%DATABASE_URL%/"$DATABASE_URL"/g" app.yaml
 
 cat app.yaml
